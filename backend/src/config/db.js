662fcf3c3dce12cfac4a12db8761f1d3ef30846b
Shetty Aditya Udaya <shettyaditya266@gmail.com>
2026-03-17 16:10:16 +0530

    Switch to manual DB configuration properties for Aiven PostgreSQL SSL fix

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: 24154,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "defaultdb",
   ssl: {
     rejectUnauthorized: false
   }
